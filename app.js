edccd1576cc3edd2b86a8085f803aceb466678bf
ronewa.github.io <newanens@gmail.com>
2024-06-10 16:37:59 +0200

    com
    1st commit

---
+++ app.js
@@ -0,0 +1,12 @@
+function openNav(){
+    document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav(){
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+// toogle nav popup
+
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
